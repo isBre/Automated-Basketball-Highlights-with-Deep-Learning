19d4587bb39e226413f8318ddb61d52ba96fb6b9
isBre <BresciaDavide@outlook.it>
2024-10-16 14:08:53 +0200

    generalized resnet generation

--- src/resnet.py
+++ src/resnet.py
@@ -1,26 +1,38 @@
-from torchvision.models import ResNet, resnet50, ResNet50_Weights
+from torchvision.models import (ResNet, ResNet50_Weights, ResNet18_Weights,
+                                resnet50, resnet18)
 import torch.nn as nn
+from torch import device
+from typing import Union
 
-def generate_resnet50(
-    pretrained=False,
-    device="cpu",
+def generate_resnet(
+        number = Union[18, 50],
+        pretrained = False,
 ) -> ResNet:
     '''
-        Generate a ResNet50 model with a sigle parameter as output
-        that is contained between [0, 1]
+        Generate a ResNet18 or ResNet50 model with a single parameter output
+        that is contained between [0, 1].
 
         Args:
-        pretrained: if true use ResNet50_Weights.DEFAULT otherwise None
-        device: str of the device used, if omitted is "cpu"
-        return:
-        resnet50 model
+        number: int, specify either 18 for ResNet18 or 50 for ResNet50.
+        pretrained: bool, if True, use pretrained weights (ResNet18_Weights.DEFAULT or ResNet50_Weights.DEFAULT), 
+        otherwise use random initialization.
+        
+        Returns:
+        A ResNet model with the final layer modified for a single output (a probability between 0 and 1).
     '''
 
-
-    if pretrained:
-        model = resnet50(weights=ResNet50_Weights.DEFAULT)
+    if number == 18:
+        if pretrained:
+            model = resnet18(weights=ResNet18_Weights.DEFAULT)
+        else:
+            model = resnet18(weights=None)
+    elif number == 50:
+        if pretrained:
+            model = resnet50(weights=ResNet50_Weights.DEFAULT)
+        else:
+            model = resnet50(weights=None)
     else:
-        model = resnet50(weights=None)
+        raise ValueError("Invalid value for 'number'. Choose either 18 or 50.")
   
     #Implementation of the last layer
     #I need a sigmoid since I eventually want to represent a probability
