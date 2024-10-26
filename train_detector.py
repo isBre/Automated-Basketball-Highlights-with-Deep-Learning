890ddfb78f134df651af3560622e45814469ca47
isBre <bresciadavide@outlook.it>
2024-10-26 11:14:57 +0200

    changed names

--- train_detector.py
+++ train_detector.py
@@ -5,11 +5,12 @@ model = YOLO("yolo11n.pt")
 
 # Train the model with the specified dataset and parameters
 train_results = model.train(
-    data="datasets/BasketLocalization/dataset.yaml",  # Path to the dataset YAML configuration
+    data="/mnt/sda1/davide/Automated-Basketball-Highlights-with-Deep-Learning/dataset/BasketLocalization/dataset.yaml",  # Path to the dataset YAML configuration
     epochs=50,  # Number of training epochs
     imgsz=640,  # Training image size (pixels)
-    device="cpu",  # Specify the device for training (CPU in this case)
+    device="cuda:0",  # Specify the device for training (CPU in this case)
     patience=5,  # Early stopping after 5 epochs without improvement
+    batch=8
 )
 
 # Export the trained model to ONNX format for deployment or further use
